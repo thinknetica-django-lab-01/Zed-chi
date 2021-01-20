5bd2843715f83d29943dae2adab0d9cf325c51b9
Zed-chi <zed-chi@mail.ru>
2021-01-20 15:29:53 +0300

    added sites flatpages to settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+SITE_ID = 1
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "moto)oaz(cww17bhyi*jly98)*_x)mhgg$s5z8^paybkx*fcyp"
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     "main",
 ]
 
