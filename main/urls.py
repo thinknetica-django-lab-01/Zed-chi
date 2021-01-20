acc0c222cf4594ccae9a11d6ff03421917fa90bb
zed <zed@mail.ru>
2021-01-20 23:41:47 +0300

    linter fix

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.flatpages import views
 from django.urls import path
+
 from .views import homepage
 
 app_name = "main"
 urlpatterns = [
-    path('', homepage, name="homepage"),
-    path('about/', views.flatpage, {'url': '/about/'}, name='about'),
-    path('contacts/', views.flatpage, {'url': '/contacts/'}, name='contacts'),
+    path("", homepage, name="homepage"),
+    path("about/", views.flatpage, {"url": "/about/"}, name="about"),
+    path("contacts/", views.flatpage, {"url": "/contacts/"}, name="contacts"),
 ]
--- marketplace/settings.py
+++ marketplace/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.sites',
-    'django.contrib.flatpages',
+    "django.contrib.sites",
+    "django.contrib.flatpages",
     "main",
 ]
 
