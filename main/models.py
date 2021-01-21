18b254e7f4e85d8ba3bcf12574036471ae7b7b1e
zed <zed@mail.ru>
2021-01-21 23:15:34 +0300

    added model with basic classes

--- main/models.py
+++ main/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField("название", max_length=50)
+    description = models.TextField("описание", max_length=300, blank=True)
+    price = models.DecimalField("цена", max_digits=8, decimal_places=2)
+    seller = models.ForeignKey()
+    category = models.ForeignKey(
+        "ProductCategory",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        verbose_name="категория",
+        related_name="products",
+    )
+
+    STATUSES = [
+        ("A", "Available"),
+        ("N", "Not Available"),
+    ]
+    status = models.CharField(choices=STATUSES, default="A", max_length=20)
+
+    class Meta:
+        verbose_name = "товар"
+        verbose_name_plural = "товары"
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(
+        "картинка",
+        upload_to="./product_images",
+    )
+    product = models.ForeignKey(
+        "Product",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        verbose_name="фото",
+        related_name="images",
+    )
+
+
+class ProductCategory(models.Model):
+    name = models.CharField("название", max_length=50)
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    title = models.CharField("тег", max_length=50)
+
+    class Meta:
+        verbose_name = "тег"
+        verbose_name_plural = "теги"
+
+    def __str__(self):
+        return self.title
+
+
+class Seller(AbstractUser):
+    company_name = models.CharField("название", max_length=50)
