908bb7e395546f5a7c816317cdc62ad82df93cd3
zed <zed@mail.ru>
2021-01-21 21:33:39 +0300

    changed media root

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_ROOT = "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 # ckeditor
