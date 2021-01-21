f1cdb83c52d89a5d7dae49e443d8560f57148cab
zed-chi <zed-chi@mail.ru>
2021-01-21 10:50:55 +0300

    changed content field to widget in flatpage admin view

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from ckeditor.widgets import CKEditorWidget
 
-# Register your models here.
+
+class FlatPageAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = FlatPage
+        fields = '__all__'
+
+FlatPageAdmin.form = FlatPageAdminForm
+
+admin.site.unregister(FlatPage)  
+admin.site.register(FlatPage, FlatPageAdmin)  
