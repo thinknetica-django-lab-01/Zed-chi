92a705f724cba37132753b30d2133365743682fb
zed <zed@mail.ru>
2021-01-26 22:33:17 +0300

    added product route

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.flatpages import views
 from django.urls import path
 
-from .views import homepage, GoodsListView
+from .views import homepage, ProductsListView, ProductDetailView
 
 app_name = "main"
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("about/", views.flatpage, {"url": "/about/"}, name="about"),
     path("contacts/", views.flatpage, {"url": "/contacts/"}, name="contacts"),
-    path("goods", GoodsListView.as_view(), name="goods_list"),
+    path("goods", ProductsListView.as_view(), name="goods_list"),
+    path("goods/<int:pk>", ProductDetailView.as_view(), name="goods_list"),
 ]
