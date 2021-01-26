5001bb49aa3112936debe94a840b66d29661107c
zed <zed@mail.ru>
2021-01-26 22:02:43 +0300

    added goods route

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.flatpages import views
 from django.urls import path
 
-from .views import homepage
+from .views import homepage, GoodsListView
 
 app_name = "main"
 urlpatterns = [
     path("", homepage, name="homepage"),
     path("about/", views.flatpage, {"url": "/about/"}, name="about"),
     path("contacts/", views.flatpage, {"url": "/contacts/"}, name="contacts"),
+    path("goods", GoodsListView.as_view(), name="goods_list"),
 ]
