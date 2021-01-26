9e6dbb7437609004f2972025415f2e856aa03131
AlexE <zed-chi@mail.ru>
2021-01-26 21:38:59 +0300

    changed if-else block to ternary

--- main/views.py
+++ main/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
 
-def homepage(request):
-    if request.user.is_authenticated:
-        username = request.user.username
-    else:
-        username = "guest"
+def homepage(request):    
+    username = request.user.username if request.user.is_authenticated else "guest"    
     return render(request, "main/index.html", {"turn_on_block":True, "name":username})
