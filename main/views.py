0865384219159567697073efd9c1553a2bb3209d
zed <zed@mail.ru>
2021-01-26 22:33:30 +0300

    added product detail view

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from .models import Product
 
@@ -10,8 +10,13 @@ def homepage(request):
         username = "guest"
     return render(request, "main/index.html", {"turn_on_block":True, "name":username})
 
-class GoodsListView(ListView):
+class ProductsListView(ListView):
     model = Product    
-    context_object_name = 'goods'
+    context_object_name = 'products'
     queryset = Product.objects.all()
     template_name = 'main/goods.html'
+
+class ProductDetailView(DetailView):
+    model = Product    
+    context_object_name = 'product'   
+    template_name = 'main/good_detail.html'    
