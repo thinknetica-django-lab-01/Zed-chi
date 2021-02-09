21d4592553e25b90f1164e033e1fd0bfde8c5d9f
zed <zed@mail.ru>
2021-02-09 23:47:33 +0300

    changed common auth url handlers

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,10 @@ from .views import (
     ProfileView,
     AddGoodView,
     GoodUpdateView,
+    SignUpView,
+    LogInView,
+    LogOutView,
+
 )
 
 app_name = "main"
@@ -19,6 +23,8 @@ urlpatterns = [
     path("goods/add", AddGoodView.as_view(), name="add_good"),
     path("goods/<int:pk>/edit", GoodUpdateView.as_view(), name="update_good"),
     path("goods/<int:pk>", ProductDetailView.as_view(), name="good_detail"),
-    path("accounts/profile/", ProfileView.as_view(), name="profile_page"),
-    path('accounts/', include('allauth.urls')),
+    path("accounts/profile/", ProfileView.as_view(), name="profile_page"),    
+    path("accounts/signup/", SignUpView.as_view(), name="sign_up_page"),
+    path("accounts/login/", LogInView.as_view(), name="log_in_page"),    
+    path("accounts/logout/", LogOutView.as_view(), name="log_out_page"),    
 ]
