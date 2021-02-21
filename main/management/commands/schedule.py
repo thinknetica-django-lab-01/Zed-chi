c893099c2373dc8534b569af6eb2ef4c75a7d525
zed-chi <zed-chi@mail.ru>
2021-02-21 22:38:35 +0300

    updated scheduler

--- main/management/commands/schedule.py
+++ main/management/commands/schedule.py
@@ -11,6 +11,8 @@ from main.models import Subscriber, Product
 from django.utils import timezone
 from datetime import timedelta
 from django.core.mail import send_mail
+from django.shortcuts import render
+from django.template import loader
 
 
 logger = logging.getLogger(__name__)
@@ -19,7 +21,11 @@ logger = logging.getLogger(__name__)
 def my_job():
     week_ago = timezone.now() - timedelta(days=7)
     products_of_the_week = Product.objects.filter(created_on__gt=week_ago)
-    
+    if not products_of_the_week:
+        return
+    content = loader.render_to_string("./week_update.html",{"products":products_of_the_week}, None)
+    for subscribe in Subscriber.objects.all():
+        send_mail("Товары недели", content, None, [subscribe.user.email,])    
 
 
 def delete_old_job_executions(max_age=604_800):
