da5f992230ed53182af98231ccee477786835e97
zed-chi <zed-chi@mail.ru>
2021-02-25 12:27:09 +0300

    added django redis settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -194,4 +194,8 @@ ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "/"
 
 # Celery
 CELERY_ALWAYS_EAGER = False #sync mode for testing without queue
-
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
